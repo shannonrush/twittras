0489dc58a42f711c22e1d19307732fddd6b20d44
Shannon Rush <shannonmrush@gmail.com>
2010-12-12 17:52:28 -0700

    xcode project and json parser

---
+++ TwittrasAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TwittrasAppDelegate.h
+//  Twittras
+//
+//  Created by Shannon Rush on 12/5/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TwittrasAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ TwittrasAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  TwittrasAppDelegate.m
+//  Twittras
+//
+//  Created by Shannon Rush on 12/5/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "TwittrasAppDelegate.h"
+
+@implementation TwittrasAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
